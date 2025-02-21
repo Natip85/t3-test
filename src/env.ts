e15cdb24d6e73bae5c1583731b2e4bd92658cb94
Natip85 <106977567+Natip85@users.noreply.github.com>
2025-02-21 13:16:15 +0200

    added resend auth

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import {createEnv} from '@t3-oss/env-nextjs'
+import {z} from 'zod'
 
 export const env = createEnv({
   /**
@@ -8,22 +8,19 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    AUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
+    AUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
     AUTH_GOOGLE_ID: z.string(),
     AUTH_GOOGLE_SECRET: z.string(),
+    AUTH_RESEND_KEY: z.string(),
+    EMAIL_FROM: z.string(),
     AUTH_REDIRECT_PROXY_URL: z.string().url().optional(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     UPLOADTHING_TOKEN: z.string(),
     UPLOADTHING_APP_ID: z.string(),
     ASSETS_PATH_PREFIX_V0: z
       .string()
-      .transform((val) => (val.startsWith("/") ? val : `/${val}`))
-      .transform((val) => (val.endsWith("/") ? val : `${val}/`)),
+      .transform((val) => (val.startsWith('/') ? val : `/${val}`))
+      .transform((val) => (val.endsWith('/') ? val : `${val}/`)),
   },
 
   /**
@@ -46,6 +43,8 @@ export const env = createEnv({
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    AUTH_RESEND_KEY: process.env.AUTH_RESEND_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM ?? 'noreply@yarcone.com',
     AUTH_REDIRECT_PROXY_URL: process.env.AUTH_REDIRECT_PROXY_URL,
     NODE_ENV: process.env.NODE_ENV,
     ASSETS_PATH_PREFIX_V0: process.env.ASSETS_PATH_PREFIX_V0,
@@ -53,7 +52,7 @@ export const env = createEnv({
     UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    NEXT_PUBLIC_IS_DEV: process.env.NODE_ENV === "development",
+    NEXT_PUBLIC_IS_DEV: process.env.NODE_ENV === 'development',
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
@@ -65,4 +64,4 @@ export const env = createEnv({
    * `SOME_VAR=''` will throw an error.
    */
   emptyStringAsUndefined: true,
-});
+})
