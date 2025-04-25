0203eabfce41a08a4742f05bca7aff0f6bf276df
Natip85 <106977567+Natip85@users.noreply.github.com>
2025-04-25 22:03:05 +0300

    dep err fix2

--- src/features/cart/cart-icon.tsx
+++ src/features/cart/cart-icon.tsx
@@ -11,7 +11,12 @@ export default function CartIcon() {
       <SheetTrigger asChild className='flex items-center justify-center hover:cursor-pointer'>
         <Toggle asChild className='size-5 rounded-full md:size-9'>
           <div className='relative'>
-            <div className='absolute right-0 top-0'>{totalQuantity}</div>
+            {totalQuantity > 0 && (
+              <div className='absolute right-0 top-0 flex size-4 items-center justify-center rounded-full bg-primary font-semibold text-white'>
+                {totalQuantity}
+              </div>
+            )}
+
             <ShoppingCartIcon />
           </div>
         </Toggle>
--- src/features/products/products-columns.tsx
+++ src/features/products/products-columns.tsx
@@ -16,26 +16,23 @@ export const columns: ColumnDef<Product>[] = [
     cell: ({row}) => <div>{row.original.id}</div>,
   },
   {
-    id: 'image', // Add an explicit 'id' field for this column
-    accessorFn: (row) => row.assets[0]?.asset.fileInfo?.url, // Extract image URL from the assets field
-    header: () => <Button variant='ghost'>Image</Button>,
+    accessorKey: 'name',
+    header: ({column}) => <DataTableColumnHeader column={column} title='Product' />,
     cell: ({row}) => {
-      const imageUrl = row.original.assets[0]?.asset.fileInfo?.url ?? '' // Use the custom accessorFn value
+      const imageUrl = row.original.assets[0]?.asset.fileInfo?.url ?? ''
       return (
-        <div className='relative aspect-square h-10 w-10'>
-          {imageUrl ? (
-            <Image src={imageUrl} alt='product img' fill className='rounded-md object-cover' />
-          ) : (
-            <ImageIcon className='size-full' />
-          )}
+        <div className='flex items-center gap-3'>
+          <div className='relative aspect-square h-10 w-10'>
+            {imageUrl ? (
+              <Image src={imageUrl} alt='product img' fill className='rounded-md object-cover' />
+            ) : (
+              <ImageIcon className='size-full' />
+            )}
+          </div>
+          <div className='flex items-center gap-2'>{row.original.name ?? ''}</div>
         </div>
       )
     },
-  },
-  {
-    accessorKey: 'name',
-    header: ({column}) => <DataTableColumnHeader column={column} title='Name' />,
-    cell: ({row}) => <div className='flex items-center gap-2'>{row.original.name ?? ''}</div>,
   },
   {
     accessorKey: 'price',
